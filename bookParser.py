dd52cbc6407c28592e23e10cb5b3f1e3ffed6d66
hspinos <hspinos@gmail.com>
2022-02-16 19:14:30 -0500

    Created base structure for book parser

---
+++ bookParser.py
@@ -0,0 +1,33 @@
+
+
+class bookParser:
+
+    def __init__(self):
+        self.bookList = []
+
+    def initList(self):
+        for i in range(4):
+            self.bookList[i] = initBook(i)
+
+    def initBook(self, index):
+        book = Book()
+        book.parseBook(index)
+
+        return book
+
+
+class Book:
+
+    def __init__(self):
+        self.title = ""
+        self.author = ""
+        self.price = ""
+
+    def parseBook(self, index):
+        parsedTitle = #parse title from returned web page
+        parsedAuthor = #parse author from returned web page
+        parsedPrice = #parse price from returned web page
+
+        self.title = parsedTitle
+        self.author = parsedAuthor
+        self.price = parsedPrice
